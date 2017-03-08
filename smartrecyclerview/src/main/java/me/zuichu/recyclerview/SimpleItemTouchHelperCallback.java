8f0ec22429ca7b4a53d9cdd9cc553c793d04cb85
zuichu <zuichutech@126.com>
2017-03-08 11:19:30 +0800

    wrapper

--- smartrecyclerview/src/main/java/me/zuichu/recyclerview/SimpleItemTouchHelperCallback.java
+++ smartrecyclerview/src/main/java/me/zuichu/recyclerview/SimpleItemTouchHelperCallback.java
@@ -59,7 +59,6 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-
         // Fade out the view as it is swiped out of the parent's bounds
         if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
             View itemView = viewHolder.itemView;
